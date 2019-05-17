0f7efdc500f42d06ff4de6faf08ef1f658cc1911
Paul Yushkevich <pyushkevich@gmail.com>
2019-05-17 12:37:42 -0400

    basic storing annot functionality working

--- slide.py
+++ slide.py
@@ -1,5 +1,5 @@
 from flask import (
-    Blueprint, flash, g, redirect, render_template, request, url_for, make_response
+    Blueprint, flash, g, redirect, render_template, request, url_for, make_response, current_app
 )
 from werkzeug.exceptions import abort
 
@@ -8,6 +8,7 @@ from flaskr.db import get_db
 from io import BytesIO
 
 import os
+import json
 
 bp = Blueprint('slide', __name__)
 
@@ -35,12 +36,31 @@ def block_detail(id):
 @bp.route('/slide/<int:id>/view', methods=('GET', 'POST'))
 def slide_view(id):
     db = get_db()
+
+    # Get the info on the current slide
     slide_info = db.execute(
         'SELECT s.*, b.specimen_name, b.block_name'
         ' FROM block b join slide s on b.id = s.block_id'
         ' WHERE s.id = ?', (id,)).fetchone()
 
-    return render_template('slide/slide_view.html', slide_id = id, slide_info = slide_info)
+    # Get the slide info
+    block_id = slide_info['block_id']
+    section = slide_info['section']
+    slideno = slide_info['slide']
+
+    # Get the previous and next slides
+    prev_slide = db.execute(
+        'SELECT id FROM slide'
+        ' WHERE block_id=? AND section <= ? AND slide <= ? AND id != ?'
+        ' ORDER BY section DESC, slide DESC limit 1', (block_id, section, slideno, id)).fetchone()
+
+    next_slide = db.execute(
+        'SELECT id FROM slide'
+        ' WHERE block_id=? AND section >= ? AND slide >= ? AND id != ?'
+        ' ORDER BY section ASC, slide ASC limit 1', (block_id, section, slideno, id)).fetchone()
+
+    return render_template('slide/slide_view.html', slide_id = id, 
+                           slide_info = slide_info, next_slide=next_slide, prev_slide=prev_slide)
 
 # Get the DZI for a slide
 @bp.route('/slide/<int:id>.dzi', methods=('GET', 'POST'))
@@ -61,13 +81,69 @@ def dzi(id):
     except KeyError:
         # Unknown slug
         abort(404)
+
+
+# Get an annotation filename for slide
+def get_annot_json_file(id):
+    
+    # Get the slide details
+    db = get_db()
+    si = db.execute(
+        'SELECT s.*, b.specimen_name, b.block_name'
+        ' FROM block b join slide s on b.id = s.block_id'
+        ' WHERE s.id = ?', (id,)).fetchone()
+
+    # Generate a file
+    json_filename = "annot_%s_%s_%s_%02d_%02d.json" % (si['specimen_name'], si['block_name'], si['stain'], si['section'], si['slide'])
+
+    # Create a directory locally
+    json_dir = os.path.join(current_app.instance_path, 'annot')
+    if not os.path.exists(json_dir):
+        os.makedirs(json_dir)
+
+    # Get the filename
+    return os.path.join(json_dir, json_filename)
+
+
+# Receive updated json for the slide
+@bp.route('/slide/<int:id>/annot/set', methods=('POST',))
+def upload_json(id):
+    # Get the raw json
+    data = json.loads(request.get_data())
+
+    # Generate a file
+    json_filename = get_annot_json_file(id)
+
+    with open(json_filename, 'w') as outfile:  
+        json.dump(data, outfile)
+
+    return json.dumps({'success':True}), 200, {'ContentType':'application/json'} 
+
+# Send the json for the slide
+@bp.route('/slide/<int:id>/annot/get', methods=('GET',))
+def get_json(id):
+
+    # Get the filename
+    json_filename = get_annot_json_file(id)
+
+    # Does it exist
+    data=None
+    if os.path.exists(json_filename):
+        with open(json_filename, 'r') as infile:
+            data=json.load(infile)
+
+    # Return the data
+    if data is not None:
+        return json.dumps(data), 200, {'ContentType':'application/json'} 
+    else:
+        return "", 200, {'ContentType':'application/json'} 
+
     
 
 class PILBytesIO(BytesIO):
     def fileno(self):
         '''Classic PIL doesn't understand io.UnsupportedOperation.'''
         raise AttributeError('Not supported')
-
 
 # Get the tiles for a slide
 @bp.route('/slide/<int:id>_files/<int:level>/<int:col>_<int:row>.<format>',  methods=('GET', 'POST'))

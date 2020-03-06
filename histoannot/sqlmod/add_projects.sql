1ebc246d11e15cbb6a48b04f1ac972c16c3d6ab6
Paul Yushkevich <pyushkevich@gmail.com>
2020-03-06 09:30:05 -0500

    Working on a database upgrade script for adding projects

--- histoannot/sqlmod/add_projects.sql
+++ histoannot/sqlmod/add_projects.sql
@@ -1,34 +1,79 @@
+/* Add new fields to user */
+ALTER TABLE user ADD COLUMN email TEXT;
+ALTER TABLE user ADD COLUMN disabled BOOLEAN NOT NULL DEFAULT (FALSE);
+ALTER TABLE user ADD COLUMN site_admin BOOLEAN NOT NULL DEFAULT(FALSE);
+
+CREATE TABLE password_reset (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  reset_key TEXT NOT NULL,
+  user INT NOT NULL,
+  t_expires INTEGER NOT NULL,
+  activated BOOLEAN NOT NULL DEFAULT(FALSE),
+  FOREIGN KEY(user) REFERENCES user(id)
+);
+
+
+/* Drop constraint on labelset */
+PRAGMA foreign_keys=off;
+BEGIN TRANSACTION;
+
+CREATE TABLE labelset_temp (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  name TEXT NOT NULL,
+  description TEXT
+);
+
+INSERT INTO labelset_temp(id,name,description)
+SELECT id,name,description
+FROM labelset;
+
+DROP TABLE labelset;
+ALTER TABLE labelset_temp RENAME TO labelset;
+COMMIT;
+PRAGMA foreign_keys=on;
+
+CREATE TABLE PROJECT (
+    id TEXT PRIMARY KEY,
+    disp_name TEXT NOT NULL,
+    desc TEXT,
+    base_url TEXT NOT NULL,
+    json TEXT NOT NULL
+);
+
 /* Unique constraint on block means it can belong to only one project */
-DROP TABLE IF EXISTS project_block;
 CREATE TABLE project_block (
   project TEXT NOT NULL,
   block INT UNIQUE NOT NULL,
   PRIMARY KEY(project,block),
+  FOREIGN KEY(project) REFERENCES project(id),
   FOREIGN KEY(block) REFERENCES block(id)
 );
 
 /* Unique constraint on task means it can belong to only one project */
-DROP TABLE IF EXISTS project_task;
 CREATE TABLE project_task (
   project TEXT NOT NULL,
   task INT UNIQUE NOT NULL,
   PRIMARY KEY(project,task),
+  FOREIGN KEY(project) REFERENCES project(id),
   FOREIGN KEY(task) REFERENCES task(id)
 );
 
-DROP TABLE IF EXISTS project_access;
 CREATE TABLE project_access (
   user INTEGER NOT NULL,
   project TEXT NOT NULL,
+  admin BOOLEAN NOT NULL DEFAULT(FALSE),
   PRIMARY KEY(user, project),
+  FOREIGN KEY(project) REFERENCES project(id),
   FOREIGN KEY (user) REFERENCES user (id)
 );
 
-/* Create a view for quick access to slide extended information */
-DROP VIEW IF EXISTS slide_info;
-CREATE VIEW slide_info AS
-   SELECT S.*, B.block_name, B.specimen_name, PB.project as project
-   FROM slide S LEFT JOIN block B on S.block_id = B.id
-                LEFT JOIN project_block PB on B.id = PB.block;
-
-
+DROP TABLE IF EXISTS project_labelset;
+CREATE TABLE project_labelset (
+  project TEXT NOT NULL,
+  labelset_name TEXT NOT NULL,
+  labelset_id INT NOT NULL,
+  PRIMARY KEY(project,labelset_name),
+  FOREIGN KEY(project) REFERENCES project(id),
+  FOREIGN KEY(labelset_name) REFERENCES labelset(name),
+  FOREIGN KEY(labelset_id) REFERENCES labelset(id)
+);

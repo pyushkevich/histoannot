bbdb55dbc637fe724929152c06998459a0620a1a
Paul Yushkevich <pyushkevich@gmail.com>
2020-06-23 08:54:44 -0400

    updated simple preprocessing docker with extra options

--- docker/histo_proc_simple/prep_bucket.sh
+++ docker/histo_proc_simple/prep_bucket.sh
@@ -22,21 +22,25 @@ function usage()
             options:
               -j <value>     : Set JPEG compression level. Default: 80
               -t <value>     : Set thumbnail size. Default: 512
+              -P             : Do not generate pyramid images. Do this if your 
+                               raw data is already in OpenSlide-compatible format
 
         process_dir: process a directory of images
 
             usage: prep_bucket process_dir [options] SRCURL TRGURL
 
             SRCURL: Google Cloud Storage URL or path with source images. 
-	            The path may also contain wildcards
+                    The path may also contain wildcards
 
             TRGURL: Google Cloud Storage URL or path for output
 
             options:
               -j <value>     : Set JPEG compression level. Default: 80
               -t <value>     : Set thumbnail size. Default: 512
-	      -n             : No-clobber mode (existing results skipped)
-	      -k <file>      : Use Google service account credentials from file
+              -P             : Do not generate pyramid images. Do this if your 
+                               raw data is already in OpenSlide-compatible format
+              -n             : No-clobber mode (existing results skipped)
+              -k <file>      : Use Google service account credentials from file
                    
 USAGE
 }
@@ -53,18 +57,20 @@ USAGE
 function process_dir()
 {
     local NOCLOBBER JPEGLEVEL THUMBSIZE SOURCE TARGET SRCLIST TRGLIST WORKDIR
-    local PYRAMID THUMB FN_BASE FN_EXT FN_NOEXT CREDS
+    local PYRAMID THUMB FN_BASE FN_EXT FN_NOEXT CREDS SKIP_PYRAMID
+    unset SKIP_PYRAMID
     unset NOCLOBBER
     JPEGLEVEL=80
     THUMBSIZE=512
 
     # Read optional arguments
-    while getopts "nj:t:k:" flag; do
+    while getopts "nj:t:k:P" flag; do
         case "$flag" in
             n) NOCLOBBER=1;;
             j) JPEGLEVEL=$OPTARG;;
             t) THUMBSIZE=$OPTARG;;
-	    k) gcloud auth activate-service-account --key-file $OPTARG;;
+            P) SKIP_PYRAMID=1;;
+            k) gcloud auth activate-service-account --key-file $OPTARG;;
         esac
     done
 
@@ -79,11 +85,11 @@ function process_dir()
     TRGLIST=$WORKDIR/target.txt
 
     # Get a list of all the images in the source directory
-    gsutil ls $SOURCE > $SRCLIST
+    gsutil ls $SOURCE | grep -v '\/$' > $SRCLIST
 
     # Get the target image list too
     if [[ $NOCLOBBER ]]; then
-        gsutil ls $TARGET/** > $TRGLIST
+        gsutil ls $TARGET/** | grep -v '\/$' > $TRGLIST
     fi
 
     while IFS= read -r FN; do
@@ -95,7 +101,12 @@ function process_dir()
             FN_EXT=$(echo $FN_BASE | sed -e "s/.*\.//g")
             FN_NOEXT=$(basename $FN_BASE .${FN_EXT})
 
-            PYRAMID="$TARGET/$FN_NOEXT/${FN_NOEXT}_pyramidal.tiff"
+            if [[ $SKIP_PYRAMID ]]; then
+                PYRAMID=""
+            else
+                PYRAMID="$TARGET/$FN_NOEXT/${FN_NOEXT}_pyramidal.tiff"
+            fi
+
             THUMB="$TARGET/$FN_NOEXT/${FN_NOEXT}_thumb.png"
 
             if grep "$PYRAMID" $TRGLIST > /dev/null \
@@ -107,7 +118,13 @@ function process_dir()
         fi
 
         # Process the image
-        process_image -j $JPEGLEVEL -t $THUMBSIZE \
+        if [[ $SKIP_PYRAMID ]]; then
+            SKIP_PYRAMID_OPT="-P"
+        else
+            SKIP_PYRAMID_OPT=""
+        fi
+
+        process_image -j $JPEGLEVEL -t $THUMBSIZE $SKIP_PYRAMID_OPT \
             "$FN" "$TARGET/$FN_NOEXT/"
 
     done < $SRCLIST
@@ -118,16 +135,18 @@ function process_image()
 {
     local JPEGLEVEL THUMBSIZE SOURCE TARGET
     local FN_BASE FN_EXT FN_NOEXT WORKDIR IMG PTIFF THUMB
-    local OPTIND OPTARG
+    local OPTIND OPTARG SKIP_PYRAMID
+    unset SKIP_PYRAMID
     JPEGLEVEL=80
     THUMBSIZE=512
 
     # Read optional arguments
-    while getopts "j:t:k:" flag; do
+    while getopts "j:t:k:P" flag; do
         case "$flag" in
             j) JPEGLEVEL=$OPTARG;;
             t) THUMBSIZE=$OPTARG;;
-	    k) gcloud auth activate-service-account --key-file $OPTARG;;
+            P) SKIP_PYRAMID=1;;
+            k) gcloud auth activate-service-account --key-file $OPTARG;;
         esac
     done
     shift $((OPTIND-1))
@@ -148,17 +167,30 @@ function process_image()
     THUMB=$WORKDIR/${FN_NOEXT}_thumb.png
     gsutil cp "$SOURCE" "$IMG"
 
-    # Perform conversion
-    vips tiffsave $IMG $PTIFF \
-        --vips-progress --compression=jpeg --Q=$JPEGLEVEL \
-        --tile --tile-width=256 --tile-height=256 \
-        --pyramid --bigtiff
+    # Already in pyramid format?
+    if [[ $SKIP_PYRAMID ]]; then
 
-    # Generate a thumbnail using openslide
-    python os_thumb.py $PTIFF $THUMB $THUMBSIZE $THUMBSIZE
+        # Generate a thumbnail using openslide
+        python os_thumb.py $IMG $THUMB $THUMBSIZE $THUMBSIZE
 
-    # Upload the result
-    gsutil cp $PTIFF $THUMB $TARGET/
+        # Upload the result
+        gsutil cp $THUMB $TARGET/
+
+    else
+
+        # Perform conversion
+        vips tiffsave $IMG $PTIFF \
+            --vips-progress --compression=jpeg --Q=$JPEGLEVEL \
+            --tile --tile-width=256 --tile-height=256 \
+            --pyramid --bigtiff
+
+        # Generate a thumbnail using openslide
+        python os_thumb.py $PTIFF $THUMB $THUMBSIZE $THUMBSIZE
+
+        # Upload the result
+        gsutil cp $PTIFF $THUMB $TARGET/
+
+    fi
 
     # Clean up
     rm -rf $WORKDIR

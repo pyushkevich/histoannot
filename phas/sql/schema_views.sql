d811300296e38c60111d97c6035b302f58971427
Paul Yushkevich <pyushkevich@gmail.com>
2026-01-13 15:28:10 -0500

    got rid of right joins

--- phas/sql/schema_views.sql
+++ phas/sql/schema_views.sql
@@ -83,8 +83,8 @@ CREATE VIEW effective_project_access AS
       SELECT PA.project, GM.user_id,
              MAX(CASE PA.access WHEN 'none' THEN 0 WHEN 'read' THEN 1 WHEN 'write' THEN 2 WHEN 'admin' THEN 3 END) AS max_num_acc, 
              MAX(api_permission) AS api_permission, MAX(anon_permission) AS anon_permission 
-      FROM effective_group_membership GM 
-      RIGHT JOIN project_access PA ON GM.group_id = PA.user 
+      FROM project_access PA
+      LEFT JOIN effective_group_membership GM ON GM.group_id = PA.user 
       GROUP BY PA.project, GM.user_id);
 
 /* A view of task access that includes group memberships */
@@ -99,6 +99,6 @@ CREATE VIEW effective_task_project_access AS
              MAX(CASE TPA.project_access WHEN 'none' THEN 0 WHEN 'read' THEN 1 WHEN 'write' THEN 2 WHEN 'admin' THEN 3 END) AS mna_project, 
              MAX(CASE TPA.task_access WHEN 'none' THEN 0 WHEN 'read' THEN 1 WHEN 'write' THEN 2 WHEN 'admin' THEN 3 END) AS mna_task, 
              MAX(api_permission) AS api_permission, MAX(anon_permission) AS anon_permission 
-      FROM effective_group_membership GM 
-      RIGHT JOIN task_project_access TPA ON GM.group_id = TPA.user 
+      FROM task_project_access TPA 
+      LEFT JOIN effective_group_membership GM ON GM.group_id = TPA.user 
       GROUP BY TPA.task, GM.user_id);
